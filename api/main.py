323e5f7858b3d1f1562827fc3dc7dff16b28330a
devops-timi <tobalereko@gmail.com>
2026-04-20 18:50:57 +0100

    fix(api/main.py): fix redis hardcoding, add missing healthendpoint

--- api/main.py
+++ api/main.py
@@ -1,22 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI HTTPException
 import redis
 import uuid
 import os
 
 app = FastAPI()
 
-r = redis.Redis(host="localhost", port=6379)
+
+r = redis.Redis(
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
+    password=os.getenv("REDIS_PASSWORD"),
+)
+
 
 @app.post("/jobs")
 def create_job():
     job_id = str(uuid.uuid4())
-    r.lpush("job", job_id)
+    QUEUE_NAME = os.getenv("QUEUE_NAME", "job")
+    r.lpush(QUEUE_NAME, job_id)
     r.hset(f"job:{job_id}", "status", "queued")
     return {"job_id": job_id}
+
 
 @app.get("/jobs/{job_id}")
 def get_job(job_id: str):
     status = r.hget(f"job:{job_id}", "status")
     if not status:
-        return {"error": "not found"}
+        raise HTTPException(status_code=404, detail="Job not found")
     return {"job_id": job_id, "status": status.decode()}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
