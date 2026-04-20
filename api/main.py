dadbbe24248af90e095be0a3a7600acae2c84ab9
devops-timi <tobalereko@gmail.com>
2026-04-20 18:52:45 +0100

    fix(api/main.py): fix redis hardcoding, add missing healthendpoint

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI HTTPException
+from fastapi import FastAPI
 import redis
 import uuid
 import os
@@ -26,7 +26,7 @@ def create_job():
 def get_job(job_id: str):
     status = r.hget(f"job:{job_id}", "status")
     if not status:
-        raise HTTPException(status_code=404, detail="Job not found")
+        return {"error": "not found"} 
     return {"job_id": job_id, "status": status.decode()}
 
 
